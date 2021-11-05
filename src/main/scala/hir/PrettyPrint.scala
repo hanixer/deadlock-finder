328bc271e6d25a1765e6c1de3027cfa5f80ce6b5
Hanixer <jijer@jiji.com>
2021-11-05 10:51:48 +0200

    Finish pretty print initial implementation

--- src/main/scala/hir/PrettyPrint.scala
+++ src/main/scala/hir/PrettyPrint.scala
@@ -2,26 +2,46 @@ package deadlockFinder
 package hir
 
 import org.typelevel.paiges.Doc
+import deadlockFinder.hir.PrettyPrint
 
-object PrettyPrint {
+object PrettyPrint:
   def apply(prog: Program, width: Int = 80): String =
-    def first = func(prog.funcs(0))
-    first.render(width)
-  
+    val first = func(prog.funcs(0))
+    first.render(0)
+
   // <FuncDecl> ::= func <name>(<name>: <typ>, ...): <type> { <body> }
-  def func(f: FuncDecl): Doc =  
+  def func(f: FuncDecl): Doc =
     val params = f.params.map(p => Doc.text(p.name) :+ ": " + Doc.str(p.typ))
-    val paramsComma = Doc.fill(Doc.text(", "), params) 
-    "func" +: Doc.str(f.name) :+ "(" + paramsComma :+ "): " + Doc.str(f.retTyp) + stmt(f.body)
+    val paramsComma = separateComma(params)
+    val body = stmt(f.body)
+    ("func " +: Doc.text(f.name))
+      + ("(" +: paramsComma :+ "): ")
+      + Doc.str(f.retTyp) + Doc.space + body
 
   def stmt(s: Stmt): Doc = s match
-    case b: Block => "{" +: Doc.fill(Doc.line, b.stmts.map(stmt)) :+ "}"
-    case a: Assignment => Doc.text(a.lhs) :+ " = "
+    case b: Block =>
+      val body = Doc.fill(Doc.line, b.stmts.map(stmt))
+      ("{" +: Doc.line) + body + (Doc.line :+ "}")
+
+    case a: Assignment => Doc.text(a.lhs) :+ " = " + expr(a.rhs)
+
+    case vd: VarDecl =>
+      var rhs = vd.rhs match
+        case Some(e) => " = " +: expr(e)
+        case _       => Doc.empty
+      ("var " + vd.name + ": ") +: (Doc.str(vd.t) + rhs)
+
     case _ => Doc.text("unsupported")
-    
+
   def expr(e: Expr): Doc = e match
     case i: IntLiteral => Doc.str(i.n)
-    case v: Variable => Doc.text(v.name)
-    case bop: BinaryOpExpr => expr(bop.lhs) + Doc.str(bop.op) + expr(bop.rhs)
-    case c: CallExpr => Doc.str(c)
-}
+    case v: Variable   => Doc.text(v.name)
+    case bop: BinaryOpExpr =>
+      expr(bop.lhs) + Doc.str(bop.op) + expr(bop.rhs)
+    case c: CallExpr =>
+      val args = separateComma(c.args.map(expr))
+      Doc.str(c) + ("(" +: args :+ ")")
+
+  def separateComma(ds: List[Doc]): Doc = Doc.fill(Doc.text(", "), ds)
+
+end PrettyPrint
--- src/main/scala/hir/ast.scala
+++ src/main/scala/hir/ast.scala
@@ -1,25 +1,36 @@
 package deadlockFinder
 package hir
 
-
 abstract sealed class AstNode {
   val loc: SourceLoc
 }
 
-case class Program(funcs: List[FuncDecl], loc: SourceLoc = SourceLoc(1, 1)) extends AstNode
+case class Program(funcs: List[FuncDecl], loc: SourceLoc = SourceLoc(1, 1))
+    extends AstNode
 
-case class FuncDecl(name: String, params: List[Param], retTyp: Type, body: Block, loc: SourceLoc) extends AstNode
+case class FuncDecl(
+    name: String,
+    params: List[Param],
+    retTyp: Type,
+    body: Block,
+    loc: SourceLoc
+) extends AstNode
 
 case class Param(name: String, typ: Type, loc: SourceLoc) extends AstNode
-
 
 trait Stmt extends AstNode
 
 case class Assignment(lhs: String, rhs: Expr, loc: SourceLoc) extends Stmt
 
-case class VarDecl(name: String, t: Type, rhs: Option[Expr], loc: SourceLoc) extends Stmt
+case class VarDecl(name: String, t: Type, rhs: Option[Expr], loc: SourceLoc)
+    extends Stmt
 
-case class IfThenElse(cond: Expr, tBranch: Stmt, eBranch: Option[Stmt], loc: SourceLoc) extends Stmt
+case class IfThenElse(
+    cond: Expr,
+    tBranch: Stmt,
+    eBranch: Option[Stmt],
+    loc: SourceLoc
+) extends Stmt
 
 case class While(cond: Expr, body: Stmt, loc: SourceLoc) extends Stmt
 
@@ -35,7 +46,6 @@ case class CallStmt(callExpr: CallExpr) extends Stmt {
   val loc: SourceLoc = callExpr.loc
 }
 
-
 trait Expr extends AstNode
 
 trait SimpleExpr extends Expr
@@ -44,35 +54,43 @@ case class IntLiteral(n: Int, loc: SourceLoc) extends SimpleExpr
 
 case class Variable(name: String, loc: SourceLoc) extends SimpleExpr
 
-case class BinaryOpExpr(op: BinaryOp, lhs: SimpleExpr, rhs: SimpleExpr, loc: SourceLoc) extends Expr
+case class BinaryOpExpr(
+    op: BinaryOp,
+    lhs: SimpleExpr,
+    rhs: SimpleExpr,
+    loc: SourceLoc
+) extends Expr
 
-case class CallExpr(name: String, args: List[SimpleExpr], loc: SourceLoc) extends Expr
-
+case class CallExpr(name: String, args: List[SimpleExpr], loc: SourceLoc)
+    extends Expr
 
 sealed trait Type
 
-case class IntType() extends Type
+case class IntType() extends Type:
+  override def toString: String = "int"
 
-case class DoubleType() extends Type
+case class DoubleType() extends Type:
+  override def toString: String = "double"
 
-case class VoidType() extends Type
+case class VoidType() extends Type:
+  override def toString: String = "void"
 
-case class ClassType() extends Type
+case class ClassType() extends Type:
+  override def toString: String = "someClass"
 
-
-enum BinaryOp {
-  case Plus, Minus, Times, Divide, Less, Greater, LessEquals, GreaterEquals, Equals, And, Or
+enum BinaryOp:
+  case Plus, Minus, Times, Divide, Less, Greater, LessEquals, GreaterEquals,
+  Equals, And, Or
 
   override def toString(): String = this match
-    case Plus => "+"
-    case Minus => "-"
-    case Times => "*"
-    case Divide => "/"
-    case Less => "<"
-    case Greater => ">"
-    case LessEquals => "<="
+    case Plus          => "+"
+    case Minus         => "-"
+    case Times         => "*"
+    case Divide        => "/"
+    case Less          => "<"
+    case Greater       => ">"
+    case LessEquals    => "<="
     case GreaterEquals => ">="
-    case Equals => "=="
-    case And => "&&"
-    case Or => "||"
-}
+    case Equals        => "=="
+    case And           => "&&"
+    case Or            => "||"
