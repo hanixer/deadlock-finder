2095dc93dd05a04bdb34e16be06717ebb81fbf8d
hanixer <a@b>
2022-03-16 17:43:53 +0200

    Add a test for petri net
    Also, add predecessors() method for petri net

--- src/main/scala/analysis/pnet/PetriNet.scala
+++ src/main/scala/analysis/pnet/PetriNet.scala
@@ -3,9 +3,14 @@ package analysis.pnet
 
 class PetriNet(val root: Place, val edges: List[Edge]):
   private val map = edges.groupMap(_.from)(_.to)
+  private val reverseMap = edges.groupMap(_.to)(_.from)
 
   val nodes: List[Node] =
     edges.flatMap(e => List(e.from, e.to)).distinct
 
   def successors(node: Node): List[Node] =
-    map(node)
+    map.getOrElse(node, List())
+
+  def predecessors(node: Node): List[Node] =
+    reverseMap.getOrElse(node, List())
+
---
+++ src/test/scala/analysis/pnet/PetriNetTest.scala
@@ -0,0 +1,43 @@
+package deadlockFinder
+package analysis.pnet
+
+import analysis.opgraph.{IntermediateNode, OperationGraphBuilder}
+import analysis.{ConstantPropagation, ProcessRank, VarInfo}
+import cfg.CfgGraph
+import common.PrettyPrint
+import translation.{HilToLil, LilToSsa, SourceToHil, Util}
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import java.nio.file.{Files, Path}
+
+class PetriNetTest extends AnyFunSuite:
+  test("basic structure") {
+    val path = "examples/parallel/MpiSendRecv.java"
+    val lil = Util.fileToSsa(path)
+    val operationGraph = OperationGraphBuilder(lil.funcs.head)
+    val petriNet = PetriNetBuilder(operationGraph)
+
+    assert(petriNet.successors(petriNet.root).length === 1)
+
+    val n1 = petriNet.successors(petriNet.root).head
+    assert(n1.isInstanceOf[Transition])
+    assert(petriNet.successors(n1).length === 4)
+    assert(petriNet.successors(n1).forall { n => n.isInstanceOf[Place] })
+
+    // Transition with two successors and two predecessors
+    assert(petriNet.nodes.exists { n =>
+      n.isInstanceOf[Transition] && petriNet.successors(n).length == 2 && petriNet.predecessors(n).length == 2
+    })
+
+    // Transition with four predecessors
+    assert(petriNet.nodes.exists { n =>
+      n.isInstanceOf[Transition] && petriNet.successors(n).length == 1 && petriNet.predecessors(n).length == 4
+    })
+
+    // Transition with four predecessors
+    assert(petriNet.nodes.exists { n =>
+      n.isInstanceOf[Place] && petriNet.successors(n).isEmpty && petriNet.predecessors(n).length == 1
+    })
+  }
+
