ba703cf0f77d5a6621ef898d9e3a6e385e0d91e6
hanixer <a@b>
2022-11-19 08:35:05 +0200

    Use short strings for ProcessRank printing

--- src/main/scala/deadlockFinder/analysis/ProcessRank.scala
+++ src/main/scala/deadlockFinder/analysis/ProcessRank.scala
@@ -5,6 +5,8 @@ enum ProcessRank:
   case AnyRank // Note, this can be mapped both to ANY_SOURCE and to process, that cannot be determined as concrete
   case Concrete(n: Int)
 
+  override def toString: String = toShortString
+
   def toShortString: String = this match
     case AnyRank => "*"
     case Concrete(n) => n.toString
